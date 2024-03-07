53ef606dc08f05ca2d5833b0ccf95529d7be2c50
Saad Taimoor <saadtaimoor@teraflare.dev>
2024-03-06 23:59:18 -0700

    Initializing Project

---
+++ src/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWiO20r_vJQs7bQM1pHdN11nD3yUcXtGU",
+  authDomain: "tfd18-justatimer.firebaseapp.com",
+  projectId: "tfd18-justatimer",
+  storageBucket: "tfd18-justatimer.appspot.com",
+  messagingSenderId: "631976735527",
+  appId: "1:631976735527:web:f9abeb75bb6754583daf02"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore
